43ddb3b9cf6424aa493e18166989a8ede9bcd2b8
Lucas Fernandes <lucas-fernandes87@hotmail.com>
2020-05-02 11:58:38 -0300

    criando as coordenadas da cobrinha

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ snack[0] = {
     y: 8*box
 }
 
+let direction = "right"
+
 function criarBG(){
     context.fillStyle = 'lightgreen';
     context.fillRect(0,0, 16 * box, 16 * box);
@@ -23,5 +25,33 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciaJogo(){
+    criarBG();
+    criarCobrinha();
+
+
+    let snack_X = snack[0].x;
+    let snack_Y = snack[0].y;
+
+    if(direction == "right") snack_X += box;
+    if(direction == "left")  snack_X  -= box;
+    if(direction == "up")  snack_Y -= box;
+    if(direction == "down") snack_Y  += box;
+
+    snack.pop();
+
+    let newHead = {
+        x: snack_X,
+        y: snack_Y
+    }
+
+    snack.unshift(newHead);
+
+}
+
+
+let jogo = setInterval(() => {
+    iniciaJogo();
+}, 100);
+
+
